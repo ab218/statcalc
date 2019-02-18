343f40449c6f402dae0a0fe3586c134ca81a60bd
A B <ab218@live.com>
2019-02-18 14:53:05 -0800

    add ac

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class App extends Component {
   state = {
     vita: 0,
-    mana: 0
+    mana: 0,
+    ac: 0
   }
 
   handleChange = name => (event) => {
@@ -14,7 +15,7 @@ class App extends Component {
   };
 
   render() {
-    const { vita, mana } = this.state;
+    const { vita, mana, ac } = this.state;
     return (
       <div className="App">
         <div className="stats">
@@ -33,6 +34,13 @@ class App extends Component {
             />
           </div>
         </div>
+        <div className="target">
+          <h3>Target AC</h3>
+          <input 
+          value={this.state.ac}
+          onChange={this.handleChange('ac')}
+          />
+        </div>
         <div className="output">
           <table>
             <tr>
@@ -41,35 +49,35 @@ class App extends Component {
             </tr>
             <tr>
               <td>LS</td>
-              <td>{Math.floor((vita * 0.5) + (mana * 2.5))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * (vita * 0.5) + (mana * 2.5))}</td> 
             </tr>
             <tr>
               <td>DA</td>
-              <td>{Math.floor((vita * 1) + (mana * 1))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * (vita * 1) + (mana * 1))}</td> 
             </tr>
             <tr>
               <td>Sam</td>
-              <td>{Math.floor((vita * 2))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * (vita * 2))}</td> 
             </tr>
             <tr>
               <td>1st Sa</td>
-              <td>{Math.floor((vita * 1.8) + (mana * 0.45))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * (vita * 1.8) + (mana * 0.45))}</td> 
             </tr>
             <tr>
               <td>2nd Sa</td>
-              <td>{Math.floor(((vita * 0.83) * 1.8) + ((mana * 0.98) * 0.45))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * ((vita * 0.83) * 1.8) + ((mana * 0.98) * 0.45))}</td> 
             </tr>
             <tr>
               <td>3rd Sa</td>
-              <td>{Math.floor(((vita * Math.pow(0.83, 2)) * 1.8) + ((mana * Math.pow(0.98, 2)) * 0.45))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * ((vita * Math.pow(0.83, 2)) * 1.8) + ((mana * Math.pow(0.98, 2)) * 0.45))}</td> 
             </tr>
             <tr>
               <td>4th Sa</td>
-              <td>{Math.floor(((vita * Math.pow(0.83, 3)) * 1.8) + ((mana * Math.pow(0.98, 3)) * 0.45))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * ((vita * Math.pow(0.83, 3)) * 1.8) + ((mana * Math.pow(0.98, 3)) * 0.45))}</td> 
             </tr>
             <tr>
               <td>5th Sa</td>
-              <td>{Math.floor(((vita * Math.pow(0.83, 4)) * 1.8) + ((mana * Math.pow(0.98, 4)) * 0.45))}</td> 
+              <td>{Math.floor((1 + (ac) / 100) * ((vita * Math.pow(0.83, 4)) * 1.8) + ((mana * Math.pow(0.98, 4)) * 0.45))}</td> 
             </tr>
           </table>
           </div>

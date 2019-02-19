722ca5dcd6e48f7c2fd72790a71f2dba385bcac8
A B <ab218@live.com>
2019-02-18 16:22:58 -0800

    add commas

--- src/App.js
+++ src/App.js
@@ -52,95 +52,95 @@ class App extends Component {
               <tr><td></td><td><h5>Rogue</h5></td></tr>
               <tr style={{color: 'blue'}}>
                 <td>LS</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.5) + (mana * 2.5)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.5) + (mana * 2.5)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.5) + (mana * 2.5)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.5) + (mana * 2.5)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'green'}}>
                 <td>DA</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 1) + (mana * 1)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 1) + (mana * 1)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 1) + (mana * 1)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 1) + (mana * 1)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'red'}}>
                 <td>Sam</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (vita * 2))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 2))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (vita * 2))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 2))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>1st Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 1.8) + (mana * 0.45)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 1.8) + (mana * 0.45)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 1.8) + (mana * 0.45)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 1.8) + (mana * 0.45)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>2nd Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (((vita * 0.83) * 1.8) + ((mana * 0.98) * 0.45)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * 0.83) * 1.8) + ((mana * 0.98) * 0.45)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (((vita * 0.83) * 1.8) + ((mana * 0.98) * 0.45)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * 0.83) * 1.8) + ((mana * 0.98) * 0.45)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>3rd Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (((vita * Math.pow(0.83, 2)) * 1.8) + ((mana * Math.pow(0.98, 2)) * 0.45)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * Math.pow(0.83, 2)) * 1.8) + ((mana * Math.pow(0.98, 2)) * 0.45)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (((vita * Math.pow(0.83, 2)) * 1.8) + ((mana * Math.pow(0.98, 2)) * 0.45)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * Math.pow(0.83, 2)) * 1.8) + ((mana * Math.pow(0.98, 2)) * 0.45)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>4th Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (((vita * Math.pow(0.83, 3)) * 1.8) + ((mana * Math.pow(0.98, 3)) * 0.45)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * Math.pow(0.83, 3)) * 1.8) + ((mana * Math.pow(0.98, 3)) * 0.45)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (((vita * Math.pow(0.83, 3)) * 1.8) + ((mana * Math.pow(0.98, 3)) * 0.45)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * Math.pow(0.83, 3)) * 1.8) + ((mana * Math.pow(0.98, 3)) * 0.45)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>5th Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (((vita * Math.pow(0.83, 4)) * 1.8) + ((mana * Math.pow(0.98, 4)) * 0.45)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * Math.pow(0.83, 4)) * 1.8) + ((mana * Math.pow(0.98, 4)) * 0.45)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (((vita * Math.pow(0.83, 4)) * 1.8) + ((mana * Math.pow(0.98, 4)) * 0.45)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (((vita * Math.pow(0.83, 4)) * 1.8) + ((mana * Math.pow(0.98, 4)) * 0.45)))).toLocaleString()}</td> 
               </tr>
               <tr><td></td><td><h5>Warrior</h5></td></tr>
               <tr style={{color: 'blue'}}>
                 <td>Berserk</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (vita * 0.85))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 0.85))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (vita * 0.85))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 0.85))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'green'}}>
                 <td>WW</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (vita * 1.575))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 1.575))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (vita * 1.575))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 1.575))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'red'}}>
                 <td>Assault</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (vita * 0.5))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 0.5))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (vita * 0.5))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (vita * 0.5))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>Sam</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 1.875 + mana * 0.5)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 1.875 + mana * 0.5)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 1.875 + mana * 0.5)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 1.875 + mana * 0.5)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'orange'}}>
                 <td>Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.4875 + mana * 0.1)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.4875 + mana * 0.1)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.4875 + mana * 0.1)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.4875 + mana * 0.1)))).toLocaleString()}</td> 
               </tr>
               <tr><td></td><td><h5>Mage</h5></td></tr>
               <tr style={{color: 'blue'}}>
                 <td>HF</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (mana * 1.8))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (mana * 1.8))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (mana * 1.8))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (mana * 1.8))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'green'}}>
                 <td>Inf</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (mana * 1.5))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (mana * 1.5))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (mana * 1.5))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (mana * 1.5))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'red'}}>
                 <td>Sam</td>
-                <td>{(Math.floor((1 + (ac / 100)) * (mana * 2.5))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (mana * 2.5))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * (mana * 2.5))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * (mana * 2.5))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'purple'}}>
                 <td>Sa</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.5 + mana * 2)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.5 + mana * 2)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.5 + mana * 2)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.5 + mana * 2)))).toLocaleString()}</td> 
               </tr>
               <tr style={{color: 'orange'}}>
                 <td>Za</td>
-                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.3 + mana * 1.8)))).toString()}</td> 
-                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.3 + mana * 1.8)))).toString()}</td> 
+                <td>{(Math.floor((1 + (ac / 100)) * ((vita * 0.3 + mana * 1.8)))).toLocaleString()}</td> 
+                <td>{(Math.floor(1.3 * (1 + (ac / 100)) * ((vita * 0.3 + mana * 1.8)))).toLocaleString()}</td> 
               </tr>
             </tbody>
           </table>

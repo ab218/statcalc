65bb61aa9cf4c549a6a7aa27c2b10adb8de68a83
A B <ab218@live.com>
2019-02-24 23:32:32 -0800

    restruc refs

---
+++ src/References.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class References extends Component {
+  render() {
+    return (
+    <div className="caveStats">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Cave/Room</th>
+                        <th>Min Vita</th>
+                        <th>Max Vita</th>
+                        <th>AC</th>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 1st Room</td>
+                        <td>750k</td>
+                        <td>1.05m</td>
+                        <td>-75</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 2nd Room</td>
+                        <td>850k</td>
+                        <td>1.05m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 3rd Room</td>
+                        <td>850k</td>
+                        <td>1.15m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 4th Room</td>
+                        <td>800k</td>
+                        <td>1.1m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 5th room</td>
+                        <td>1.37m</td>
+                        <td>1.75m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 6th room</td>
+                        <td>1.4m</td>
+                        <td>1.8m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 7th room</td>
+                        <td>1.5m</td>
+                        <td>2m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - 8th room</td>
+                        <td>1.8m</td>
+                        <td>2.25m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Bird4 - Last room</td>
+                        <td>1.75m</td>
+                        <td>2.25m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>Turtle4 - Last room</td>
+                        <td>1.8m</td>
+                        <td>2.5m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>ATG 9</td>
+                        <td>2.2m</td>
+                        <td>2.85m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>ATG 10</td>
+                        <td>3.3m</td>
+                        <td>4.3m</td>
+                        <td>-76</td>
+                    </tr>
+                    <tr>
+                        <td>ATG 11</td>
+                        <td>4.5m</td>
+                        <td>5.7m</td>
+                        <td>-76</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+  }
+}
+
+export default References;

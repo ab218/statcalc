a0c88fbcc85f19f2590d774ca3e07e6a07b7e823
A B <ab218@live.com>
2019-11-17 00:06:01 -0800

    references mapping from contentful

--- src/App.js
+++ src/App.js
@@ -1,203 +1,234 @@
 import React from 'react';
+import { createClient } from 'contentful';
 import './App.css';
-import Output from './Output'
-import Stats from './Stats'
-import Target from './Target'
-import EXP from './Experience'
-import References from './References'
+import Output from './Output';
+import Stats from './Stats';
+import Target from './Target';
+import EXP from './Experience';
+import References from './References';
 
 export default class App extends React.PureComponent {
-  state = {
-    ac: '-76',
-    vita: '0',
-    mana: '0',
-    desiredVita: '0',
-    desiredMana: '0',
-    vitaExp: '0',
-    manaExp: '0',
-    desiredVitaExp: '0',
-    desiredManaExp: '0',
-    exp: '0',
-    dh: false,
-    sc: false,
-    sleep: false,
-  }
+	state = {
+		ac: '-76',
+		vita: '0',
+		mana: '0',
+		desiredVita: '0',
+		desiredMana: '0',
+		vitaExp: '0',
+		manaExp: '0',
+		desiredVitaExp: '0',
+		desiredManaExp: '0',
+		exp: '0',
+		dh: false,
+		sc: false,
+		sleep: false,
+		references: [],
+	};
 
-  addSc = () => {
-    if (this.state.sc) {
-      return this.setState(prevState => {
-        return { ac: (parseInt(prevState.ac) - 50).toString(), sc: false }
-      })
-    }
-    return this.setState(prevState => {
-      return { ac: (parseInt(prevState.ac) + 50).toString(), sc: true }
-    })
-  }
+	componentDidMount = () => {
+		if (!process.env.CONTENTFUL_SPACE) {
+			return;
+		}
+		const client = createClient({
+			space: process.env.CONTENTFUL_SPACE,
+			accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+		});
+		client
+			.getEntries()
+			.then((response) => {
+				const found = response.items.find((item) => item.fields.hasOwnProperty('list'));
+				this.setState({ references: found.fields.list });
+			})
+			.catch(console.error);
+	};
 
-  addDh = () => {
-    if (this.state.dh) {
-      return this.setState(prevState => {
-        return { ac: (parseInt(prevState.ac) - 12).toString(), dh: false }
-      })
-    }
-    return this.setState(prevState => {
-      return { ac: (parseInt(prevState.ac) + 12).toString(), dh: true }
-    })
-  }
+	addSc = () => {
+		if (this.state.sc) {
+			return this.setState((prevState) => {
+				return { ac: (parseInt(prevState.ac) - 50).toString(), sc: false };
+			});
+		}
+		return this.setState((prevState) => {
+			return { ac: (parseInt(prevState.ac) + 50).toString(), sc: true };
+		});
+	};
 
-  addSleep = () => {
-    if (this.state.sleep) {
-      return this.setState({ sleep: false })
-    }
-    return this.setState({ sleep: true });
-  }
+	addDh = () => {
+		if (this.state.dh) {
+			return this.setState((prevState) => {
+				return { ac: (parseInt(prevState.ac) - 12).toString(), dh: false };
+			});
+		}
+		return this.setState((prevState) => {
+			return { ac: (parseInt(prevState.ac) + 12).toString(), dh: true };
+		});
+	};
 
-  setAc = (ac) => {
-    this.setState({ ac, sc: false, dh: false })
-  }
+	addSleep = () => {
+		if (this.state.sleep) {
+			return this.setState({ sleep: false });
+		}
+		return this.setState({ sleep: true });
+	};
 
-  handleChange = name => (event) => {
-    this.setState({
-      [name]: event.target.value.replace(/[^\d-]+/g, ''),
-    });
-  };
+	setAc = (ac) => {
+		this.setState({ ac, sc: false, dh: false });
+	};
 
-  handleVita = () => (event) => {
-    const { value } = event.target;
-    const vCost  = Math.floor(((((Math.floor(value/20000))*4)-((Math.floor(value/20000)-6)*2))));
-    const vSells = Math.floor(value/100);
-    const vSegments = Math.floor((vSells-1000)/(200));
-    let exp;
-    if (value < 100000) {
-      exp = (value / 100) * 20
-    } else if (value >= 100000) {
-      exp = (vSells * 20) + (((((2*vSegments)+20+22)/2)-20) * (vSegments*200)) + ((vSells-1000-(vSegments * 200)) * (vCost-20))
-    } else {
-      exp = null
-    }
-    this.setState({
-      vita: value.replace(/[^\d,]+/g, ''),
-      vitaExp: exp
-    });
-  };
+	handleChange = (name) => (event) => {
+		this.setState({
+			[name]: event.target.value.replace(/[^\d-]+/g, ''),
+		});
+	};
 
-  handleMana = () => (event) => {
-    const { value } = event.target;
-    const mCost  = Math.floor(((((Math.floor(value/10000))*4)-((Math.floor(value/10000)-6)*2))))
-    const mSells = Math.floor(value/50)
-    const mSegments = Math.floor((mSells-1000)/(200));
-    let exp;
-    if (value < 50000) {
-      exp = (value / 50) * 20
-    } else if (value >= 50000) {
-      exp = (mSells * 20) + (((((2 * mSegments) + 20 + 22) / 2) - 20) * (mSegments*200)) + ((mSells - 1000 - (mSegments * 200)) * (mCost-20))
-    } else {
-      exp = null
-    }
-    this.setState({
-      mana: value.replace(/[^\d,]+/g, ''),
-      manaExp: exp
-    });
-  };
+	handleVita = () => (event) => {
+		const { value } = event.target;
+		const vCost = Math.floor(Math.floor(value / 20000) * 4 - (Math.floor(value / 20000) - 6) * 2);
+		const vSells = Math.floor(value / 100);
+		const vSegments = Math.floor((vSells - 1000) / 200);
+		let exp;
+		if (value < 100000) {
+			exp = value / 100 * 20;
+		} else if (value >= 100000) {
+			exp =
+				vSells * 20 +
+				((2 * vSegments + 20 + 22) / 2 - 20) * (vSegments * 200) +
+				(vSells - 1000 - vSegments * 200) * (vCost - 20);
+		} else {
+			exp = null;
+		}
+		this.setState({
+			vita: value.replace(/[^\d,]+/g, ''),
+			vitaExp: exp,
+		});
+	};
 
-  handleDesiredVita = () => (event) => {
-    const { value } = event.target;
-    const vCost = Math.floor(((((Math.floor(value/20000))*4)-((Math.floor(value/20000)-6)*2))))
-    const vSells = Math.floor(value/100)
-    const vSegments = Math.floor((vSells-1000)/(200));
-    let exp;
-    if (value < 100000) {
-      exp = (value / 100) * 20
-    } else if (value >= 100000) {
-      exp = (vSells * 20) + (((((2*vSegments)+20+22)/2)-20) * (vSegments*200)) + ((vSells-1000-(vSegments*200)) * (vCost-20))
-    } else {
-      exp = null
-    }
-    this.setState({
-      desiredVita: value.replace(/[^\d,]+/g, ''),
-      desiredVitaExp: exp
-    });
-  };
+	handleMana = () => (event) => {
+		const { value } = event.target;
+		const mCost = Math.floor(Math.floor(value / 10000) * 4 - (Math.floor(value / 10000) - 6) * 2);
+		const mSells = Math.floor(value / 50);
+		const mSegments = Math.floor((mSells - 1000) / 200);
+		let exp;
+		if (value < 50000) {
+			exp = value / 50 * 20;
+		} else if (value >= 50000) {
+			exp =
+				mSells * 20 +
+				((2 * mSegments + 20 + 22) / 2 - 20) * (mSegments * 200) +
+				(mSells - 1000 - mSegments * 200) * (mCost - 20);
+		} else {
+			exp = null;
+		}
+		this.setState({
+			mana: value.replace(/[^\d,]+/g, ''),
+			manaExp: exp,
+		});
+	};
 
-  handleDesiredMana = () => (event) => {
-    const { value } = event.target;
-    const mCost  = Math.floor(((((Math.floor(value/10000))*4)-((Math.floor(value/10000)-6)*2))))
-    const mSells = Math.floor(value/50)
-    const mSegments = Math.floor((mSells-1000)/(200));
-    let exp;
-    if (value < 50000) {
-      exp = (value / 50) * 20
-    } else if (value >= 50000) {
-      exp = (mSells * 20) + (((((2*mSegments)+20+22)/2)-20) * (mSegments*200)) + ((mSells-1000-(mSegments*200)) * (mCost-20))
-    } else {
-      exp = null
-    }
-    this.setState({
-      desiredMana: value.replace(/[^\d,]+/g, ''),
-      desiredManaExp: exp
-    });
-  };
+	handleDesiredVita = () => (event) => {
+		const { value } = event.target;
+		const vCost = Math.floor(Math.floor(value / 20000) * 4 - (Math.floor(value / 20000) - 6) * 2);
+		const vSells = Math.floor(value / 100);
+		const vSegments = Math.floor((vSells - 1000) / 200);
+		let exp;
+		if (value < 100000) {
+			exp = value / 100 * 20;
+		} else if (value >= 100000) {
+			exp =
+				vSells * 20 +
+				((2 * vSegments + 20 + 22) / 2 - 20) * (vSegments * 200) +
+				(vSells - 1000 - vSegments * 200) * (vCost - 20);
+		} else {
+			exp = null;
+		}
+		this.setState({
+			desiredVita: value.replace(/[^\d,]+/g, ''),
+			desiredVitaExp: exp,
+		});
+	};
 
-  withSleep = () => {
-    if (this.state.sleep) {
-      return 1.3
-    }
-    return 1;
-  }
+	handleDesiredMana = () => (event) => {
+		const { value } = event.target;
+		const mCost = Math.floor(Math.floor(value / 10000) * 4 - (Math.floor(value / 10000) - 6) * 2);
+		const mSells = Math.floor(value / 50);
+		const mSegments = Math.floor((mSells - 1000) / 200);
+		let exp;
+		if (value < 50000) {
+			exp = value / 50 * 20;
+		} else if (value >= 50000) {
+			exp =
+				mSells * 20 +
+				((2 * mSegments + 20 + 22) / 2 - 20) * (mSegments * 200) +
+				(mSells - 1000 - mSegments * 200) * (mCost - 20);
+		} else {
+			exp = null;
+		}
+		this.setState({
+			desiredMana: value.replace(/[^\d,]+/g, ''),
+			desiredManaExp: exp,
+		});
+	};
 
-  render() {
-    const {
-      vita,
-      mana,
-      desiredVita,
-      desiredMana,
-      ac,
-      dh,
-      sc,
-      vitaExp,
-      manaExp,
-      desiredVitaExp,
-      desiredManaExp
-    } = this.state;
-    return (
-      <div className="App">
-        <Stats
-          vita={vita}
-          mana={mana}
-          desiredVita={desiredVita}
-          desiredMana={desiredMana}
-          handleVita={this.handleVita}
-          handleMana={this.handleMana}
-          handleDesiredVita={this.handleDesiredVita}
-          handleDesiredMana={this.handleDesiredMana}
-        />
-        <Target
-          ac={ac}
-          addDh={this.addDh}
-          addSc={this.addSc}
-          addSleep={this.addSleep}
-          dh={dh}
-          handleChange={this.handleChange}
-          sc={sc}
-          setAc={this.setAc}
-        />
-        <EXP
-          vitaExp={vitaExp}
-          manaExp={manaExp}
-          desiredVitaExp={desiredVitaExp}
-          desiredManaExp={desiredManaExp}
-        />
-        <Output
-          vita={vita.replace(/\D/g, '')}
-          mana={mana.replace(/\D/g, '')}
-          desiredVita={desiredVita.replace(/\D/g, '')}
-          desiredMana={desiredMana.replace(/\D/g, '')}
-          ac={ac}
-          withSleep={this.withSleep}
-        />
-        <h3>Reference</h3>
-        <References />
-      </div>
-    );
-  }
+	withSleep = () => {
+		if (this.state.sleep) {
+			return 1.3;
+		}
+		return 1;
+	};
+
+	render() {
+		const {
+			vita,
+			mana,
+			desiredVita,
+			desiredMana,
+			ac,
+			dh,
+			sc,
+			vitaExp,
+			manaExp,
+			desiredVitaExp,
+			desiredManaExp,
+			references,
+		} = this.state;
+		return (
+			<div className="App">
+				<Stats
+					vita={vita}
+					mana={mana}
+					desiredVita={desiredVita}
+					desiredMana={desiredMana}
+					handleVita={this.handleVita}
+					handleMana={this.handleMana}
+					handleDesiredVita={this.handleDesiredVita}
+					handleDesiredMana={this.handleDesiredMana}
+				/>
+				<Target
+					ac={ac}
+					addDh={this.addDh}
+					addSc={this.addSc}
+					addSleep={this.addSleep}
+					dh={dh}
+					handleChange={this.handleChange}
+					sc={sc}
+					setAc={this.setAc}
+				/>
+				<EXP vitaExp={vitaExp} manaExp={manaExp} desiredVitaExp={desiredVitaExp} desiredManaExp={desiredManaExp} />
+				<Output
+					vita={vita.replace(/\D/g, '')}
+					mana={mana.replace(/\D/g, '')}
+					desiredVita={desiredVita.replace(/\D/g, '')}
+					desiredMana={desiredMana.replace(/\D/g, '')}
+					ac={ac}
+					withSleep={this.withSleep}
+				/>
+				{references.length > 0 && (
+					<React.Fragment>
+						<h3>Reference</h3>
+						<References references={references} />
+					</React.Fragment>
+				)}
+			</div>
+		);
+	}
 }
--- src/References.js
+++ src/References.js
@@ -1,101 +1,33 @@
 import React from 'react';
 import './App.css';
 
-export default function References() {
-  const styles = {
-    increase: {fontWeight: 'bold', color: 'green'}
-  }
-  return (
-    <div className="caveStats">
-      <table>
-        <tbody>
-          <tr>
-            <th>Cave[Room]</th>
-            <th>Min Vita</th>
-            <th>Max Vita</th>
-            <th>Base AC</th>
-          </tr>
-          <tr>
-            <td>Bird4[1]</td>
-            <td>750k</td>
-            <td>1.05m</td>
-            <td>-75</td>
-          </tr>
-          <tr>
-            <td>Bird4[2]</td>
-            <td>850k</td>
-            <td>1.05m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[3]</td>
-            <td>850k</td>
-            <td>1.15m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[4]</td>
-            <td>800k</td>
-            <td>1.1m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[5]</td>
-            <td>1.37m</td>
-            <td>1.75m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[6]</td>
-            <td>1.4m</td>
-            <td>1.8m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[7]</td>
-            <td>1.5m</td>
-            <td>2m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[8]</td>
-            <td>1.8m</td>
-            <td>2.25m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Bird4[LR]</td>
-            <td>1.7m</td>
-            <td>2.15m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>Turtle4[LR]</td>
-            <td>1.8m</td>
-            <td>2.5m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>ATG 9</td>
-            <td>2.2m</td>
-            <td>2.85m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>ATG 10</td>
-            <td>3.3m</td>
-            <td>4.3m</td>
-            <td>-76</td>
-          </tr>
-          <tr>
-            <td>ATG 11</td>
-            <td style={styles.increase}>5.2m</td>
-            <td style={styles.increase}>6.5m</td>
-            <td>-76</td>
-            <td style={styles.increase}>+15% increase</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+export default function References({ references }) {
+	const mapReferences = (references) => {
+		return references.map((ref) => {
+			const { caveName, min, max, ac } = ref.fields;
+			return (
+				<tr key={caveName}>
+					<td>{caveName}</td>
+					<td>{min}</td>
+					<td>{max}</td>
+					<td>{ac}</td>
+				</tr>
+			);
+		});
+	};
+	return (
+		<div className="caveStats">
+			<table>
+				<tbody>
+					<tr>
+						<th>Cave[Room]</th>
+						<th>Min Vita</th>
+						<th>Max Vita</th>
+						<th>Base AC</th>
+					</tr>
+					{mapReferences(references)}
+				</tbody>
+			</table>
+		</div>
+	);
 }

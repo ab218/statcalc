e24ee0ee28e7e92dff9af7d674cda3146c5cf971
A B <ab218@live.com>
2020-11-24 10:49:33 -0800

    ref.fields

--- src/References.js
+++ src/References.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function References({ references }) {
-	const mapReferences = (references) => {
-		return references.map((ref) => {
-			const { caveName, min, max, ac } = ref.fields;
-			if (ref && ref.fields && ref.fields.caveName) {
-				return (
-					<tr key={caveName}>
-						<td>{caveName}</td>
-						<td>{min}</td>
-						<td>{max}</td>
-						<td>{ac}</td>
-					</tr>
-				);
-			} else {
-				return null;
-			}
-		});
-	};
-	return (
-		<div className="cave-stats">
-			<table>
-				<tbody>
-					<tr>
-						<th>Cave[Room]</th>
-						<th>Min Vita</th>
-						<th>Max Vita</th>
-						<th>Base AC</th>
-					</tr>
-					{mapReferences(references)}
-				</tbody>
-			</table>
-		</div>
-	);
+  const mapReferences = (references) => {
+    return references.map((ref) => {
+      if (ref && ref.fields && ref.fields.caveName) {
+        const { caveName, min, max, ac } = ref.fields;
+        return (
+          <tr key={caveName}>
+            <td>{caveName}</td>
+            <td>{min}</td>
+            <td>{max}</td>
+            <td>{ac}</td>
+          </tr>
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+  return (
+    <div className="cave-stats">
+      <table>
+        <tbody>
+          <tr>
+            <th>Cave[Room]</th>
+            <th>Min Vita</th>
+            <th>Max Vita</th>
+            <th>Base AC</th>
+          </tr>
+          {mapReferences(references)}
+        </tbody>
+      </table>
+    </div>
+  );
 }

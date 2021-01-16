85bd8ba6df6aabc0bfd1abfc43fb18adcf531afe
A B <ab218@live.com>
2021-01-15 19:42:11 -0800

    Remove dead code

--- src/Output.js
+++ src/Output.js
@@ -1,70 +1,52 @@
-import React from 'react';
-import './App.css';
-import Mage from './Mage.js';
-import Rogue from './Rogue.js';
-import Warrior from './Warrior.js';
+import React from "react";
+import "./App.css";
+import Mage from "./Mage.js";
+import Rogue from "./Rogue.js";
+import Warrior from "./Warrior.js";
 
-export default function Output({ vita, mana, desiredVita, desiredMana, ac, withSleep }) {
-	function SpellRow({
-		color,
-		spellName,
-		vita,
-		mana,
-		vitaMultiplyer,
-		manaMultiplyer,
-		desiredVita,
-		desiredMana,
-		withAc,
-		withSleep,
-	}) {
-		return (
-			<tr style={{ color }}>
-				<td>{spellName}</td>
-				<td>{Math.floor(withSleep() * (withAc * (vita * vitaMultiplyer + mana * manaMultiplyer))).toLocaleString()}</td>
-				<td>
-					{Math.floor(
-						withSleep() * (withAc * (desiredVita * vitaMultiplyer + desiredMana * manaMultiplyer)),
-					).toLocaleString()}
-				</td>
-			</tr>
-		);
-	}
-
-	function determineAC(armor) {
-		const ac = Number(armor);
-		if (ac >= -95) {
-			return 1 + ac / 100;
-		} else {
-			const adjAc = ac + 95;
-			return (1 - 95 / 100) * (1 + adjAc / 100);
-		}
-	}
-	return (
-		<div className="output">
-			<Rogue
-				vita={vita}
-				mana={mana}
-				desiredVita={desiredVita}
-				desiredMana={desiredMana}
-				withAc={determineAC(ac)}
-				withSleep={withSleep}
-			/>
-			<Warrior
-				vita={vita}
-				mana={mana}
-				desiredVita={desiredVita}
-				desiredMana={desiredMana}
-				withAc={determineAC(ac)}
-				withSleep={withSleep}
-			/>
-			<Mage
-				vita={vita}
-				mana={mana}
-				desiredVita={desiredVita}
-				desiredMana={desiredMana}
-				withAc={determineAC(ac)}
-				withSleep={withSleep}
-			/>
-		</div>
-	);
+export default function Output({
+  vita,
+  mana,
+  desiredVita,
+  desiredMana,
+  ac,
+  withSleep,
+}) {
+  function determineAC(armor) {
+    const ac = Number(armor);
+    if (ac >= -95) {
+      return 1 + ac / 100;
+    } else {
+      const adjAc = ac + 95;
+      return (1 - 95 / 100) * (1 + adjAc / 100);
+    }
+  }
+  return (
+    <div className="output">
+      <Rogue
+        vita={vita}
+        mana={mana}
+        desiredVita={desiredVita}
+        desiredMana={desiredMana}
+        withAc={determineAC(ac)}
+        withSleep={withSleep}
+      />
+      <Warrior
+        vita={vita}
+        mana={mana}
+        desiredVita={desiredVita}
+        desiredMana={desiredMana}
+        withAc={determineAC(ac)}
+        withSleep={withSleep}
+      />
+      <Mage
+        vita={vita}
+        mana={mana}
+        desiredVita={desiredVita}
+        desiredMana={desiredMana}
+        withAc={determineAC(ac)}
+        withSleep={withSleep}
+      />
+    </div>
+  );
 }
